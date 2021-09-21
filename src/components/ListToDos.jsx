5baaeae0418ee97ff8362e16a48659baea4cea68
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-09-21 18:33:32 -0300

    Separating components and making tasks to be listed in reverse order

--- src/components/ListToDos.jsx
+++ src/components/ListToDos.jsx
@@ -1,8 +1,8 @@
-import { Container, Box, IconButton, Grid, GridItem } from '@chakra-ui/react';
-import { DeleteIcon, CheckIcon } from '@chakra-ui/icons';
+import { Container, Box, Grid, GridItem } from '@chakra-ui/react';
 import React, { useContext, useEffect } from 'react';
 import ToDoContext from '../context/ToDoContext';
 import RemoveToDo from './RemoveToDo';
+import ToggleToDo from './ToggleToDo';
 
 const ListToDos = () => {
   const { toDos, getToDos } = useContext(ToDoContext);
@@ -15,7 +15,7 @@ const ListToDos = () => {
     <>
       <Container maxW='container.md'>
         {toDos
-          ? toDos.map((toDo) => (
+          ? [...toDos].reverse().map((toDo) => (
               <Grid
                 key={toDo.title}
                 templateColumns='repeat(5, 1fr)'
@@ -38,19 +38,7 @@ const ListToDos = () => {
                   <Box fontSize='1rem'>{toDo.title}</Box>
                 </GridItem>
                 <GridItem colStart={6} colEnd={6}>
-                  <Box
-                    position={'absolute'}
-                    right={'4rem'}
-                    _hover={{
-                      transition: 'transform .2s',
-                      transform: 'scale(1.05)',
-                    }}
-                  >
-                    <IconButton
-                      variant='ghost'
-                      icon={<CheckIcon color='green.500' />}
-                    />
-                  </Box>
+                  <ToggleToDo toDo={toDo} />
                   <RemoveToDo toDo={toDo} />
                 </GridItem>
               </Grid>
