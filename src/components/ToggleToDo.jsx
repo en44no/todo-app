a2d37eee7605f6a80a3769d4e4c85a3709d029d2
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-09-21 18:35:08 -0300

    New component to mark tasks as completed

---
+++ src/components/ToggleToDo.jsx
@@ -0,0 +1,47 @@
+import React, { useContext } from 'react';
+import { Box, IconButton, useToast } from '@chakra-ui/react';
+import { CheckIcon } from '@chakra-ui/icons';
+import ToDoContext from '../context/ToDoContext';
+
+const ToggleToDo = (toDo) => {
+  const { getToDos, toggleToDo } = useContext(ToDoContext);
+  const toast = useToast();
+
+  const toggle = () => {
+    toggleToDo(toDo.toDo);
+    const id = 4;
+    if (!toast.isActive(id)) {
+      toast({
+        id: 4,
+        title: 'Tarea completada.',
+        description: 'Has marcado la tarea como completada.',
+        status: 'success',
+        duration: 5000,
+        position: 'bottom-right',
+        isClosable: true,
+      });
+      setTimeout(() => {
+        toast.close();
+      }, 5000);
+    }
+    getToDos();
+  };
+
+  return (
+    <Box
+      onClick={() => {
+        toggle();
+      }}
+      position={'absolute'}
+      right={'4rem'}
+      _hover={{
+        transition: 'transform .2s',
+        transform: 'scale(1.05)',
+      }}
+    >
+      <IconButton variant='ghost' icon={<CheckIcon color='green.500' />} />
+    </Box>
+  );
+};
+
+export default ToggleToDo;
