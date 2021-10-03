fb16975abc0c9d77950b92bbff27457336d842c4
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-03 13:44:45 -0300

    Fixed console error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useColorMode } from '@chakra-ui/color-mode';
 import { SunIcon, MoonIcon } from '@chakra-ui/icons';
-import { Container, Heading, Button, Box, IconButton } from '@chakra-ui/react';
+import { Container, Heading, Button, Box } from '@chakra-ui/react';
 import React from 'react';
 import FilterToDo from './FilterToDo';
 
@@ -13,36 +13,42 @@ const Header = () => {
         <Box d={'block'}>
           <Heading as='h2' size='xl'>
             Mis tareas
-            <Button
-              boxShadow='base'
-              ml='1rem'
-              w={'5%'}
-              _hover={{
-                bg: bgColor,
-                transition: 'transform .2s',
-                transform: 'scale(1.02)',
-              }}
-              aria-label='darkMode'
-              onClick={toggleColorMode}
-            >
-              {colorMode === 'light' ? (
-                <IconButton
-                  backgroundColor={'#dfe6e9'}
-                  _hover={{ bg: '#edf2f7' }}
-                  aria-label='lightModeButton'
-                >
-                  <SunIcon color='#000' />
-                </IconButton>
-              ) : (
-                <IconButton
-                  backgroundColor={'#1a202c'}
-                  _hover={{ bg: '#2c313d' }}
-                  aria-label='darkModeButton'
-                >
-                  <MoonIcon color='#fff' />
-                </IconButton>
-              )}
-            </Button>
+            {colorMode === 'light' ? (
+              <Button
+                boxShadow='base'
+                backgroundColor={'#dfe6e9'}
+                aria-label='lightModeButton'
+                ml='1rem'
+                w={'5%'}
+                _hover={{
+                  bg: bgColor,
+                  transition: 'transform .2s',
+                  transform: 'scale(1.02)',
+                  boxShadow: 'base',
+                }}
+                _focus={{ boxShadow: 'base' }}
+                onClick={toggleColorMode}
+              >
+                <SunIcon color='#000' />
+              </Button>
+            ) : (
+              <Button
+                boxShadow='base'
+                ml='1rem'
+                w={'5%'}
+                _hover={{
+                  bg: bgColor,
+                  transition: 'transform .2s',
+                  transform: 'scale(1.02)',
+                }}
+                _focus={{ boxShadow: 'base' }}
+                aria-label='darkMode'
+                onClick={toggleColorMode}
+                backgroundColor={'#1a202c'}
+              >
+                <MoonIcon color='#fff' />
+              </Button>
+            )}
             <FilterToDo />
           </Heading>
         </Box>
