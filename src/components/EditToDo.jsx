91ba0c71a8189887ea77782264fb42e35389bd01
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-03 16:34:44 -0300

    Added notifications and validations to editToDo

--- src/components/EditToDo.jsx
+++ src/components/EditToDo.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { Editable, EditableInput, EditablePreview } from '@chakra-ui/react';
 import ToDoContext from '../context/ToDoContext';
+import Notification from './Notification';
 
 const EditToDo = ({ toDoToEdit }) => {
   const [value, setValue] = useState(toDoToEdit);
@@ -9,14 +10,26 @@ const EditToDo = ({ toDoToEdit }) => {
   const oldTitle = toDoToEdit;
 
   const editToDoPrivate = (oldTitle, newTitle) => {
-    editToDo(oldTitle, newTitle);
+    if (oldTitle !== newTitle && newTitle !== '') {
+      Notification(
+        'Tarea editada correctamente.',
+        'Has editado la tarea.',
+        'success'
+      );
+      editToDo(oldTitle, newTitle);
+    }
+    if (newTitle === '') {
+      setValue(oldTitle);
+      Notification(
+        'Error al editar tarea.',
+        'Debes agregar un título la tarea.',
+        'error'
+      );
+    }
   };
   return (
     <>
-      <Editable
-        value={value ? value : 'Enter a value'}
-        onSubmit={() => editToDoPrivate(oldTitle, value)}
-      >
+      <Editable value={value} onSubmit={() => editToDoPrivate(oldTitle, value)}>
         <EditablePreview />
         <EditableInput
           onChange={(e) => setValue(e.target.value)}
