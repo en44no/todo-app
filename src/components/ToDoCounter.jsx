7f59a56a2db28523234ed8e992a98a86dd95113d
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-10-24 19:11:01 -0300

    Now you can know how many tasks you have pending and completed

---
+++ src/components/ToDoCounter.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import { Text } from '@chakra-ui/react';
+import ToDoContext from '../context/ToDoContext';
+
+const ToDoCounter = (props) => {
+  const { toDos } = useContext(ToDoContext);
+  const completedTodos = toDos.filter((t) => t.completed === true).length;
+  const pendingTodos = toDos.filter((t) => t.completed === false).length;
+
+  return (
+    <>
+      <Text
+        fontSize='1.03rem'
+        mt='1rem'
+        mb='-0.5rem'
+        ml='0.2rem'
+        fontWeight='400'
+      >
+        Tienes
+        <span
+          style={{
+            color: props.type === 'pendientes' ? '#D69E2E' : '#38A169',
+            fontWeight: '600',
+          }}
+        >
+          {props.type === 'pendientes' ? (
+            <span> {pendingTodos} </span>
+          ) : (
+            <span> {completedTodos} </span>
+          )}
+        </span>
+        {(props.type === 'pendientes' && pendingTodos === 1) ||
+        (props.type === 'completadas' && completedTodos === 1)
+          ? 'tarea '
+          : 'tareas '}
+        {props.type === 'completadas'
+          ? props.type === 'completadas' && completedTodos === 1
+            ? `completada.`
+            : `${props.type}.`
+          : props.type === 'pendientes' && pendingTodos === 1
+          ? `pendiente.`
+          : `${props.type}.`}
+      </Text>
+    </>
+  );
+};
+
+export default ToDoCounter;
